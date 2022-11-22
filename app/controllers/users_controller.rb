5f824c27b983be05f8ff1b8a3099448dc37f59be
Ayano Hoshi <114926497+hoshikokoko@users.noreply.github.com>
2022-11-22 14:19:00 +0000

    [Add]4-26

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,12 @@
 class UsersController < ApplicationController
+  def show
+    @users = User.find(params[:id])
+  end
+  
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :profile_image)
+  end
 end

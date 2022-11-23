a33d750340d23dc42c8037d9d308f536997a1933
Ayano Hoshi <114926497+hoshikokoko@users.noreply.github.com>
2022-11-23 12:37:10 +0000

    [Add]4-51

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,7 +1,7 @@
 class Book < ApplicationRecord
   
   validates :title, presence: true
-  validates :body, presence: true
+  validates :body, presence: true, length: { minimum: 0, maximum: 200 }
   
   has_one_attached :image
   belongs_to :user
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,9 @@ class User < ApplicationRecord
   
   has_many :books, dependent: :destroy
   
-  validates :name, presence: true
+  validates :name, presence: true, length: { minimum: 2, maximum: 20 }
+  validates :name, uniqueness: true
+  validates :introduction,  length: { minimum: 0, maximum: 50 }
   
   has_one_attached :profile_image
   

623b897352c9f25312c70407d84342c9514bd8fb
Ayano Hoshi <114926497+hoshikokoko@users.noreply.github.com>
2022-11-23 06:01:52 +0000

    [Add]4-28

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,13 +8,17 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     @book.save
-    redirect_to books_path
+    redirect_to book_path(@book.id)
+  end
+  
+  def show
+    @book = Book.find(params[:id])
   end
   
   
   private
 
   def book_params
-    params.require(:book).permit(:title, :image, :body)
+    params.require(:book).permit(:title, :body, :image)
   end
 end
